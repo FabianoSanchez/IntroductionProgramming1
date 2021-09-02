44cc4fc2670f227e24d0714deb748b9edf1d9891
Fabiano <fabissb@hotmail.com>
2021-09-02 13:55:26 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            name = Console.Readline();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
